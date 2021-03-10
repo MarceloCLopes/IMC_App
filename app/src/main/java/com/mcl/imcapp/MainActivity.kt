27e82237ebb0c057d45d7f1195df78d9e557c36b
MarceloCLopes <marc01mcl@gmail.com>
2021-03-10 16:23:25 -0300

    updated main activity, created functions set listener and calcular imc

--- app/src/main/java/com/mcl/imcapp/MainActivity.kt
+++ app/src/main/java/com/mcl/imcapp/MainActivity.kt
@@ -2,10 +2,37 @@ package com.mcl.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.main_constraint.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_constraint)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        editAltura.doAfterTextChanged { text ->
+            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        editPeso.doOnTextChanged { text, _, _, _ ->
+            textTitle.text = text
+        }
+        btnCalcular.setOnClickListener {
+            calcularIMC(editPeso.text.toString(), editAltura.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if (peso != null && altura != null){
+            val imc = peso / (altura * altura)
+            textTitle.text = "Seu IMC é:\n%.2f".format(imc)
+        }
     }
 }
